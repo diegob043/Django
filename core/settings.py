cd94da2805ffe9e3ca1922577db6d370a010db9b
diegob043 <ampuero.diegob@gmail.com>
2022-12-27 15:37:13 -0300

    implementación login con css e imagenes

--- core/settings.py
+++ core/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#Variables de Ambiente
+SECRET_KEY = 'django-insecure-#ualgckcpcpq8+_w_-5px0pkmxfll)by+z%9-046e%51-8h1bf'  #os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True #os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
