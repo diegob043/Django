013284fcbab5615668cbaa007f79c8efed66ac56
diegob043 <ampuero.diegob@gmail.com>
2022-12-28 15:17:26 -0300

    Comentarios y edicion Nico

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Este script permite implementar y ejecutar tareas administrativas"""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    """Esta función permite ejecutar tareas administrativas para la aplicación"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -16,7 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
